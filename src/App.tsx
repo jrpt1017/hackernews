7c863c0554117ccd2ce41439a6c64d1c8f88994c
jonnelrodjiptolosa <jonnelrodji.tolosa@hcl.com>
2021-05-01 00:55:10 +0800

    Added dashboard component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
 import './App.css';
 
 const App: React.FC<{}> = () => {
   return (
     <div className="App">
+      <Header />
       hello world
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Dashboard/Dashboard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const Dashboard: React.FC<{}> = () => {
+  return (
+    <div className="App">
+      Sample Content
+    </div>
+  );
+}
+
+export default Dashboard;
