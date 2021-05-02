b596c8e4974211eb0b52f9b7a1c2edabfa6c31a9
jonnelrodjiptolosa <jonnelrodji.tolosa@hcl.com>
2021-05-02 23:41:11 +0800

    Update service to use Promise all when fetching news one by one

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,21 +1,25 @@
 import axios from 'axios';
 
-export const getNews = async () => {
-  try {
-    const news = await axios.get('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty');
-    return news.data;
-  } catch (error) {
-    console.log(error.message)
-    return error.message;
-  }
-};
-
 export const getNewsByID = async (newsID: number) => {
   try {
     const news = await axios.get(`https://hacker-news.firebaseio.com/v0/item/${newsID}.json?print=pretty`);
     return news.data;
   } catch (error) {
     console.log(error.message);
+    return error.message;
+  }
+};
+
+export const getNews = async () => {
+  try {
+    const news = await axios.get('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty');
+    const newsArray = await Promise.all(news.data.slice(0, 5).map(async (newsId: number) => {
+      return await getNewsByID(newsId);
+    }));
+    console.log('newsArray', newsArray)
+    return newsArray;
+  } catch (error) {
+    console.log(error.message)
     return error.message;
   }
 };
